9138a6aeb936da1b515ebd9ca63df9fd81501bda
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-05-26 16:49:09 +0900

    add toggle

--- momentum/app.js
+++ momentum/app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector("#title");
 
 function handleTitleClick() {
-    const clicked = "active";
-    if (h1.classList.contains(clicked)) {
-        h1.classList.remove(clicked);
-    } else {
-        h1.classList.add(clicked);
-    }
+    h1.classList.toggle("active");
 }
 
 h1.addEventListener("click", handleTitleClick);
