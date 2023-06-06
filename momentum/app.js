e923dbd271a86259a68137aaacd2b414041e0463
jh0152park <jh0152park@naver.com>
2023-06-06 19:02:30 +0900

    added localStorage getItem

--- momentum/app.js
+++ momentum/app.js
@@ -2,13 +2,29 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const sayHello = document.querySelector("#hello");
 
+const NAME = "username";
+const HIDDEN_CLASS = "hidden";
+
+const savedUsername = localStorage.getItem(NAME);
+
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
-  loginForm.classList.add("hidden");
+  loginForm.classList.add(HIDDEN_CLASS);
 
-  sayHello.innerText = `Hello ${username}!`;
-  sayHello.classList.toggle("hidden");
+  localStorage.setItem(NAME, username);
+  printHello();
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function printHello() {
+  username = localStorage.getItem(NAME);
+  sayHello.innerText = `Hello ${username}!`;
+  sayHello.classList.remove(HIDDEN_CLASS);
+}
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  printHello();
+}
