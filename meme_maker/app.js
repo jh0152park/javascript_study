280cb2c69058a2a5a8329583c5a0f4438bed48e5
jh0152park <jh0152park@naver.com>
2023-06-12 19:06:38 +0900

    added color buttons

--- meme_maker/app.js
+++ meme_maker/app.js
@@ -1,6 +1,11 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 const line_width = document.getElementById("line_width");
+const color = document.getElementById("color");
+const color_option = Array.from(
+  document.getElementsByClassName("color_option")
+);
+
 var isPainting = false;
 
 canvas.width = 800;
@@ -46,9 +51,21 @@ function onLineWidthChange(event) {
   context.lineWidth = event.target.value;
 }
 
+function onColorChange(event) {
+  context.strokeStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  console.dir(event.target.dataset.color);
+  context.strokeStyle = event.target.dataset.color;
+}
+
 canvas.addEventListener("mousemove", onMousemove);
 canvas.addEventListener("mousedown", onMousedown);
 canvas.addEventListener("mouseup", onMouseup);
 canvas.addEventListener("mouseleave", onMouseleave);
 
 line_width.addEventListener("change", onLineWidthChange);
+
+color.addEventListener("change", onColorChange);
+color_option.forEach((color) => color.addEventListener("click", onColorClick));
