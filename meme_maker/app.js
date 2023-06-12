14e7ccfaba91152ef76857e9e622e0357b836148
jh0152park <jh0152park@naver.com>
2023-06-12 16:15:55 +0900

    draw house

---
+++ meme_maker/app.js
@@ -0,0 +1,16 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+context.fillRect(200, 200, 50, 200);
+context.fillRect(400, 200, 50, 200);
+context.lineWidth = 2;
+context.strokeRect(300, 300, 50, 100);
+
+context.fillRect(200, 200, 200, 20);
+context.moveTo(200, 200);
+context.lineTo(325, 100);
+context.lineTo(450, 200);
+context.fill();
