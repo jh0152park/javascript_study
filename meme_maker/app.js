43f98c5262340c15d42884dabd33ace8586401a1
jh0152park <jh0152park@naver.com>
2023-06-12 16:32:33 +0900

    created human

--- meme_maker/app.js
+++ meme_maker/app.js
@@ -4,13 +4,19 @@ const context = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-context.fillRect(200, 200, 50, 200);
-context.fillRect(400, 200, 50, 200);
-context.lineWidth = 2;
-context.strokeRect(300, 300, 50, 100);
+context.fillRect(220, 200, 15, 100);
+context.fillRect(380, 200, 15, 100);
+context.fillRect(260, 200, 100, 100);
 
-context.fillRect(200, 200, 200, 20);
-context.moveTo(200, 200);
-context.lineTo(325, 100);
-context.lineTo(450, 200);
+context.arc(310, 150, 40, 0, 360);
+context.fill();
+
+context.beginPath();
+context.fillStyle = "white";
+context.arc(290, 140, 8, 0, 360);
+context.arc(330, 140, 8, 0, 360);
+context.fill();
+
+context.beginPath();
+context.arc(310, 165, 15, 0, 1.0 * Math.PI);
 context.fill();
