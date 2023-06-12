b2aa7e55f8612f0112a8d0ee88bbf5a05a5a0b99
jh0152park <jh0152park@naver.com>
2023-06-12 16:45:50 +0900

    created onClick function

--- meme_maker/app.js
+++ meme_maker/app.js
@@ -4,19 +4,13 @@ const context = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-context.fillRect(220, 200, 15, 100);
-context.fillRect(380, 200, 15, 100);
-context.fillRect(260, 200, 100, 100);
+context.lineWidth = 2;
 
-context.arc(310, 150, 40, 0, 360);
-context.fill();
+function onClick(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  context.lineTo(x, y);
+  context.stroke();
+}
 
-context.beginPath();
-context.fillStyle = "white";
-context.arc(290, 140, 8, 0, 360);
-context.arc(330, 140, 8, 0, 360);
-context.fill();
-
-context.beginPath();
-context.arc(310, 165, 15, 0, 1.0 * Math.PI);
-context.fill();
+canvas.addEventListener("click", onClick);
